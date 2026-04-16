c6b33e154650408b511bfeee28c12499c66b1bfc
Nithin Reddy <nithinreddy34261@gmail.com>
2026-04-16 13:56:20 +0530

    feat: add Booking Entity for Bookings

--- bookmyshow/src/main/java/com/nithin/bookmyshow/model/Booking.java
+++ bookmyshow/src/main/java/com/nithin/bookmyshow/model/Booking.java
@@ -1,26 +1,35 @@
 package com.nithin.bookmyshow.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "booking")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private BigDecimal price;
+    private BigDecimal totalAmount;
     @Column(nullable = false)
     private LocalDateTime bookingTime;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BookingStatus bookingStatus;
     @JoinColumn(name = "user_id")
     @ManyToOne
     private User user;
     @JoinColumn(name = "seat_id")
     @ManyToOne
     private Seat seat;
+    @JoinColumn(name = "show_id")
+    @ManyToOne
+    private Show show;
 }
---
+++ bookmyshow/src/main/java/com/nithin/bookmyshow/model/BookingStatus.java
@@ -0,0 +1,7 @@
+package com.nithin.bookmyshow.model;
+
+public enum BookingStatus {
+    CONFIRMED,
+    CANCELLED,
+    PENDING
+}
